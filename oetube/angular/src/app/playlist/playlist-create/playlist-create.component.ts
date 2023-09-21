8487f352b270a515a2d5cfc0e391dbc183763c82
Tamás Török <torokt21@gmail.com>
2023-09-21 14:23:05 +0200

    Add playlist create component

---
+++ oetube/angular/src/app/playlist/playlist-create/playlist-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-create',
+  templateUrl: './playlist-create.component.html',
+  styleUrls: ['./playlist-create.component.scss']
+})
+export class PlaylistCreateComponent {
+
+}
