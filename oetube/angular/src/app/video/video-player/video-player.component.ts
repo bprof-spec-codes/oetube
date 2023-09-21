298e5f8c901ba0e7c8ad870097e9b653cf75a89b
Tamás Török <torokt21@gmail.com>
2023-09-21 14:23:58 +0200

    Add video player component

---
+++ oetube/angular/src/app/video/video-player/video-player.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss']
+})
+export class VideoPlayerComponent {
+
+}
