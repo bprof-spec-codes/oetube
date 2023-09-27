f2b7d34b72502a8ec11653deb3d9e74962693a76
Benjámin Béres <beresb0@gmail.com>
2023-09-27 20:43:39 +0200

    VideoAppService added for experimental purposes

---
+++ oetube/aspnet-core/OeTube/Application/Services/VideoAppService.cs
@@ -0,0 +1,23 @@
+﻿using OeTube.Domain.Storages;
+using System.Net;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Content;
+
+namespace OeTube.Application.Services
+{
+    public class VideoAppService:ApplicationService
+    {
+        private readonly VideoStorageService _videoStorageService;
+
+        public VideoAppService(VideoStorageService videoStorageService)
+        {
+            _videoStorageService = videoStorageService;
+        }
+
+        public async Task UploadVideoAsync(IRemoteStreamContent content)
+        {
+             Guid id = GuidGenerator.Create();
+            await _videoStorageService.SaveVideoAsync(id, content);
+        }
+    }
+}
