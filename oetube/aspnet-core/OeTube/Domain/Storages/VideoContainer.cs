250045363687020d94e5946fe0a719c01968e346
beresB <57649310+beresB@users.noreply.github.com>
2023-09-28 16:06:22 +0200

    folder structure refactored

--- oetube/aspnet-core/OeTube/Domain/Storages/VideoContainer.cs
+++
@@ -1,7 +0,0 @@
-﻿using Volo.Abp.BlobStoring;
-using Volo.Abp.Domain.Repositories;
-
-namespace OeTube.Domain.Storages
-{
-   
-}
--- oetube/aspnet-core/OeTube/External/FFmpeg/StreamInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OeTube.External.FFmpeg
-{
-    public abstract class StreamInfo
-    {
-        public string Codec { get; init; }
-        public TimeSpan Duration { get; init; }
-        public long Bitrate { get; init; }
-    }
-}
