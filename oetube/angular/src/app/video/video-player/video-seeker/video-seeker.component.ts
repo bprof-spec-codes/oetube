809855492a3bfcc684ff568391fb15c4f0ed4816
Tamás Török <torokt21@gmail.com>
2023-10-02 16:00:07 +0200

    Add video seeker

---
+++ oetube/angular/src/app/video/video-player/video-seeker/video-seeker.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { ValueChangedEvent } from 'devextreme/ui/progress_bar';
+
+@Component({
+  selector: 'app-video-seeker',
+  templateUrl: './video-seeker.component.html',
+  styleUrls: ['./video-seeker.component.scss'],
+})
+export class VideoSeekerComponent {
+  @Input() max = 0;
+  @Input() value = 0;
+  @Input() label = '';
+  @Output() inputChange = new EventEmitter<ValueChangedEvent>();
+  @Output() valueChange = new EventEmitter<ValueChangedEvent>();
+
+  inputHandler(event: ValueChangedEvent) {
+    this.inputChange.emit(event);
+  }
+
+  changeHandler(event: ValueChangedEvent) {
+    this.valueChange.emit(event);
+  }
+}
