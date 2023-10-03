469de15a1796807c6428320ad7bfcf8252627e73
beresB <57649310+beresB@users.noreply.github.com>
2023-10-03 18:26:59 +0200

    Database mapping added.

---
+++ oetube/aspnet-core/OeTube/Data/Configurations/VideoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OeTube.Data.Configurations
+{
+    public class VideoConfiguration : IEntityTypeConfiguration<Video>
+    {
+        public void Configure(EntityTypeBuilder<Video> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.OwnsMany(p => p.AccessGroups, action =>
+            {
+                builder.HasOne(typeof(Group)).WithMany().HasForeignKey(nameof(AccessGroup.Id));
+            })
+                .HasOne(typeof(IdentityUser<Guid>)).WithMany().HasForeignKey(nameof(Video.CreatorId));
+            builder.Property(p => p.AccessGroups).HasField("_accessGroups");
+        }
+    }
+}
