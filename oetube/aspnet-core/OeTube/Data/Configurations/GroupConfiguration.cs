c596c1531f17e5c152db5cb20067bb10b0d482a5
beresB <57649310+beresB@users.noreply.github.com>
2023-10-03 18:26:59 +0200

    Database mapping added.

---
+++ oetube/aspnet-core/OeTube/Data/Configurations/GroupConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OeTube.Data.Configurations
+{
+    public class GroupConfiguration : IEntityTypeConfiguration<Group>
+    {
+        public void Configure(EntityTypeBuilder<Group> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.OwnsMany(p => p.Members, action =>
+            {
+                builder.HasOne(typeof(IdentityUser<Guid>)).WithMany().HasForeignKey(nameof(Member.Id));
+            })
+                .HasOne(typeof(IdentityUser<Guid>)).WithMany().HasForeignKey(nameof(Group.CreatorId));
+            builder.Property(p => p.Members).HasField("_members");
+        }
+    }
+}
