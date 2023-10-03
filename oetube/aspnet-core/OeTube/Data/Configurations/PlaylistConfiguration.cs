c13a6bf0131b66dce608da414c49e4128d2ebab0
beresB <57649310+beresB@users.noreply.github.com>
2023-10-03 18:26:59 +0200

    Database mapping added.

---
+++ oetube/aspnet-core/OeTube/Data/Configurations/PlaylistConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OeTube.Data.Configurations
+{
+    public class PlaylistConfiguration : IEntityTypeConfiguration<Playlist>
+    {
+        public void Configure(EntityTypeBuilder<Playlist> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.OwnsMany(p => p.Items, action =>
+            {
+                builder.HasOne(typeof(Video)).WithMany().HasForeignKey(nameof(VideoItem.Id));
+            })
+                .HasOne(typeof(IdentityUser<Guid>)).WithMany().HasForeignKey(nameof(Playlist.CreatorId));
+            builder.Property(p => p.Items).HasField("_items");
+        }
+    }
+}
