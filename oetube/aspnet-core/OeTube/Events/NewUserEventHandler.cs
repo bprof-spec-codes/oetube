975eef921cddbb24f92c36eb4783ebd9d7f8f905
Benjámin Béres <beresb0@gmail.com>
2023-10-07 01:00:51 +0200

    NewUserEventHandler added.

---
+++ oetube/aspnet-core/OeTube/Events/NewUserEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using OeTube.Domain.Repositories;
+using OeTube.Entities;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events;
+using Volo.Abp.Domain.Entities.Events.Distributed;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.EventBus;
+using Volo.Abp.Identity;
+
+namespace OeTube.Events
+{
+    public class NewUserEventHandler : ILocalEventHandler<EntityCreatedEventData<IdentityUser>>,ITransientDependency
+    {
+        private readonly IOeTubeUserRepository _users;
+
+        public NewUserEventHandler(IOeTubeUserRepository users)
+        {
+            _users = users;
+        }
+
+        public async Task HandleEventAsync(EntityCreatedEventData<IdentityUser> eventData)
+        {
+            await _users.InsertAsync(new OeTubeUser(eventData.Entity),true);
+        }
+    }
+}
