f342ade594537e83c0f18322ea1a27080085b797
Benjámin Béres <beresb0@gmail.com>
2023-10-07 00:48:49 +0200

    Playlist and VideoItem database configuration done.

--- oetube/aspnet-core/OeTube/Data/Configurations/PlaylistConfiguration.cs
+++ oetube/aspnet-core/OeTube/Data/Configurations/PlaylistConfiguration.cs
@@ -1,20 +1,46 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Autofac.Features.OwnedInstances;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OeTube.Domain.Entities.Playlists;
+using OeTube.Domain.Entities.Videos;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+using Volo.Abp.Identity;
 
 namespace OeTube.Data.Configurations
 {
+
     public class PlaylistConfiguration : IEntityTypeConfiguration<Playlist>
     {
         public void Configure(EntityTypeBuilder<Playlist> builder)
         {
+            builder.ConfigureByConvention();
             builder.HasKey(p => p.Id);
-            builder.OwnsMany(p => p.Items, action =>
-            {
-                builder.HasOne(typeof(Video)).WithMany().HasForeignKey(nameof(VideoItem.Id));
-            })
-                .HasOne(typeof(IdentityUser<Guid>)).WithMany().HasForeignKey(nameof(Playlist.CreatorId));
-            builder.Property(p => p.Items).HasField("_items");
+            
+            builder.Property(p => p.Name)
+                   .HasMaxLength(PlaylistConstants.NameMinLength)
+                   .IsRequired();
+
+            builder.Property(p => p.Description)
+                   .HasMaxLength(PlaylistConstants.DescriptionMaxLength);
+
+            builder.Ignore(p => p.Items);
+            builder.HasMany(p => p.Items)
+                   .WithOne()
+                   .HasForeignKey(nameof(VideoItem.PlaylistId))
+                   .OnDelete(DeleteBehavior.Cascade);
+            builder.ConfigureCreator<IdentityUser,Playlist>();
+            builder.ConfigureCreationTimeIndex();
+        }
+    }
+    public class VideoItemConfiguration : IEntityTypeConfiguration<VideoItem>
+    {
+        public void Configure(EntityTypeBuilder<VideoItem> builder)
+        {
+            builder.HasKey(nameof(VideoItem.PlaylistId), nameof(VideoItem.Order));
+            builder.HasOne(typeof(Video))
+                   .WithMany()
+                   .HasForeignKey(nameof(VideoItem.VideoId))
+                   .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
