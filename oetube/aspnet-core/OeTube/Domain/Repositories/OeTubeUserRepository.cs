ba5b6eda6c5365d7b9a0894966cce51622032966
Benjámin Béres <beresb0@gmail.com>
2023-10-07 00:56:30 +0200

    OeTubeRepository added.

---
+++ oetube/aspnet-core/OeTube/Domain/Repositories/OeTubeUserRepository.cs
@@ -0,0 +1,31 @@
+﻿using OeTube.Data;
+using OeTube.Domain.Entities.Groups;
+using OeTube.Entities;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace OeTube.Domain.Repositories
+{
+    public interface IReadOnlyOeTubeUserRepository : IReadOnlyRepository<OeTubeUser, Guid>
+    {
+        Task<IQueryable<OeTubeUser>> GetUsersByEmailDomainAsync(string emailDomain);
+    }
+    public interface IOeTubeUserRepository:IReadOnlyOeTubeUserRepository,IRepository<OeTubeUser,Guid>
+    { }
+
+    [ExposeServices(typeof(IReadOnlyOeTubeUserRepository),typeof(IOeTubeUserRepository))]
+    public class OeTubeUserRepository 
+        : EfCoreRepository<OeTubeDbContext, OeTubeUser, Guid>,IOeTubeUserRepository,ITransientDependency
+    {
+        public OeTubeUserRepository(IDbContextProvider<OeTubeDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+
+        }
+        public async Task<IQueryable<OeTubeUser>> GetUsersByEmailDomainAsync(string emailDomain)
+        {
+            return (await GetQueryableAsync()).Where(u => u.EmailDomain == emailDomain);
+        }
+    }
+}
