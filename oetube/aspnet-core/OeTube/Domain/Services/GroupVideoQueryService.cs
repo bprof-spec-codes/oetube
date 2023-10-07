09870aeeb2e531acc76cff498c96c4da78521df3
beresB <57649310+beresB@users.noreply.github.com>
2023-10-07 13:59:30 +0200

    GroupVideoQueryService added.

---
+++ oetube/aspnet-core/OeTube/Domain/Services/GroupVideoQueryService.cs
@@ -0,0 +1,45 @@
+﻿using OeTube.Domain.Entities.Groups;
+using OeTube.Domain.Entities.Videos;
+using OeTube.Domain.Repositories;
+using Volo.Abp.Domain.Services;
+
+namespace OeTube.Domain.Services
+{
+    public interface IGroupVideoQueryService
+    {
+        Task<IQueryable<Group>> GetAccessGroupsAsync(Video video);
+        Task<IQueryable<Video>> GetAvaliableVideosAsync(Group group);
+    }
+
+    public class GroupVideoQueryService : DomainService, IGroupVideoQueryService
+    {
+        private readonly IReadOnlyGroupRepository _groups;
+        private readonly IReadOnlyVideoRepository _videos;
+
+        public GroupVideoQueryService(IReadOnlyGroupRepository groups, IReadOnlyVideoRepository videos)
+        {
+            _groups = groups;
+            _videos = videos;
+        }
+
+        public async Task<IQueryable<Group>> GetAccessGroupsAsync(Video video)
+        {
+            var result = from accessGroup in await _videos.GetAccessGroupsQueryableAsync()
+                         where accessGroup.VideoId == video.Id
+                         join @group in await _groups.GetQueryableAsync()
+                         on accessGroup.GroupId equals @group.Id
+                         select @group;
+            return result;
+        }
+        public async Task<IQueryable<Video>> GetAvaliableVideosAsync(Group group)
+        {
+            var result = from accessGroup in await _videos.GetAccessGroupsQueryableAsync()
+                         where accessGroup.GroupId == @group.Id
+                         join video in await _videos.GetQueryableAsync()
+                         on accessGroup.VideoId equals video.Id
+                         select video;
+            return result;
+        }
+    }
+
+}
