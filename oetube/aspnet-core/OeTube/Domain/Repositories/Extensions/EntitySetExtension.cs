b6d426ce1eda7aeeac34a664904384399b5e1e74
beresB <57649310+beresB@users.noreply.github.com>
2023-10-10 11:26:40 +0200

    Extension methods added.

---
+++ oetube/aspnet-core/OeTube/Domain/Repositories/Extensions/EntitySetExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OeTube.Entities;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace OeTube.Domain.Repositories.Extensions
+{
+    public static class EntitySetExtension
+    {
+        public static async Task<EntitySet<TEntity, TKey>> GetManyAsSetAsync<TEntity, TKey>(this IRepository<TEntity, TKey> repository, IEnumerable<TKey> keys, bool includeDetails = false, CancellationToken cancellationToken = default)
+        where TEntity : class, IEntity<TKey>, IHasAtomicKey<TKey>
+         where TKey : notnull
+        {
+            var result = await repository.GetManyQueryableAsync(keys, includeDetails);
+            return await result.ToEntitySetAsync<TEntity, TKey>(cancellationToken);
+        }
+        public static async Task<EntitySet<TEntity, TKey>> ToEntitySetAsync<TEntity, TKey>(this IQueryable<TEntity> queryable, CancellationToken cancellationToken = default)
+         where TEntity : class, IEntity<TKey>, IHasAtomicKey<TKey>
+         where TKey : notnull
+        {
+
+            var set = new EntitySet<TEntity, TKey>();
+            await foreach (var element in queryable.AsAsyncEnumerable().WithCancellation(cancellationToken))
+            {
+                set.Add(element);
+            }
+
+            return set;
+        }
+    }
+
+}
---
+++ oetube/aspnet-core/OeTube/Domain/Repositories/Extensions/QueryableExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUglify;
+using OeTube.Entities;
+using System.Linq.Expressions;
+using System.Threading;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.BackgroundWorkers;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.ObjectMapping;
+using Volo.Abp.TenantManagement.EntityFrameworkCore;
+
+namespace OeTube.Domain.Repositories.Extensions
+{
+    public static class QueryableExtension
+    {
+
+        public static IQueryable<T> ToPagedAndSorted<T>(this IQueryable<T> queryable,
+            IPagedAndSortedResultRequest request)
+        {
+            return queryable.OrderByIf<T, IQueryable<T>>(!request.Sorting.IsNullOrWhiteSpace(), request.Sorting)
+                            .PageBy(request.SkipCount, request.MaxResultCount);
+        }
+        public static PagedResultDto<TDestination> ToPagedResultDto<TSource, TDestination>
+            (this IQueryable<TSource> queryable, 
+            IObjectMapper mapper, IPagedAndSortedResultRequest request,bool includeDetails = false)
+            where TSource : class, IEntity
+        {
+            var result = queryable.Include(includeDetails)
+                                  .ToPagedAndSorted(request)
+                                  .Select(mapper.Map<TSource, TDestination>)
+                                  .ToList();
+            return new PagedResultDto<TDestination>(result.Count, result);
+        }
+    }
+}
