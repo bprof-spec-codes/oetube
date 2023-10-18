ed4fb319003b8d8912dc79d3aad89d418f5d5e42
Tamás Török <torokt21@gmail.com>
2023-10-18 15:00:46 +0200

    Simulate search

--- oetube/angular/src/app/video/video-grid/video-grid/video-grid.component.ts
+++ oetube/angular/src/app/video/video-grid/video-grid/video-grid.component.ts
@@ -18,8 +18,8 @@ export type Video = {
   styleUrls: ['./video-grid.component.scss'],
 })
 export class VideoGridComponent implements OnInit {
-  //TODO make this @input
   public videos$: Observable<Video[]>;
+  public isLoading: boolean;
 
   ngOnInit(): void {
     this.refreshVideos();
@@ -31,48 +31,57 @@ export class VideoGridComponent implements OnInit {
 
   refreshVideos(searchPhrase?: string) {
     //Observable from Create Method
-    this.videos$ = of([
-      {
-        Id: '1',
-        Name: 'Test video',
-        Description: 'Placeholder description',
-        Duration: 5000,
-        CreationTime: new Date(),
-        CreatorId: 'guid',
-      } as Video,
-      {
-        Id: '2',
-        Name: 'Placeholder video',
-        Description: 'Something witty',
-        Duration: 20345,
-        CreationTime: new Date(),
-        CreatorId: 'guid2',
-      } as Video,
-      {
-        Id: '3',
-        Name: 'Informative video',
-        Description: 'Something witty',
-        Duration: 30,
-        CreationTime: new Date(),
-        CreatorId: 'guid2',
-      } as Video,
-      {
-        Id: '4',
-        Name: 'Just another movie',
-        Description: 'With a nice description',
-        Duration: 20345,
-        CreationTime: new Date(),
-        CreatorId: 'guid2',
-      } as Video,
-      {
-        Id: '5',
-        Name: 'Just another video with a rather long title which should never fit on the users screen',
-        Description: 'With a nice description',
-        Duration: 20345,
-        CreationTime: new Date(),
-        CreatorId: 'guid2',
-      } as Video,
-    ]);
+    this.videos$ = undefined;
+    this.isLoading = true;
+
+    // TODO use service
+    // This is only to simulate loading times
+    setTimeout(() => {
+      this.videos$ = of([
+        {
+          Id: '1',
+          Name: 'Test video',
+          Description: 'Placeholder description',
+          Duration: 5000,
+          CreationTime: new Date(),
+          CreatorId: 'guid',
+        } as Video,
+        {
+          Id: '2',
+          Name: 'Placeholder video',
+          Description: 'Something witty',
+          Duration: 20345,
+          CreationTime: new Date(),
+          CreatorId: 'guid2',
+        } as Video,
+        {
+          Id: '3',
+          Name: 'Informative video',
+          Description: 'Something witty',
+          Duration: 30,
+          CreationTime: new Date(),
+          CreatorId: 'guid2',
+        } as Video,
+        {
+          Id: '4',
+          Name: 'Just another movie',
+          Description: 'With a nice description',
+          Duration: 20345,
+          CreationTime: new Date(),
+          CreatorId: 'guid2',
+        } as Video,
+        {
+          Id: '5',
+          Name: 'Just another video with a rather long title which should never fit on the users screen',
+          Description: 'With a nice description',
+          Duration: 20345,
+          CreationTime: new Date(),
+          CreatorId: 'guid2',
+        } as Video,
+      ]);
+
+      this.isLoading = false;
+    }, 1000 + Math.random() * 3000);
   }
 
   fromatVideoDuration(duration: number) {
--- oetube/angular/src/app/video/video.module.ts
+++ oetube/angular/src/app/video/video.module.ts
@@ -1,9 +1,15 @@
-import { DxButtonModule, DxSliderModule, DxTextBoxModule } from 'devextreme-angular';
+import {
+  DxButtonModule,
+  DxLoadIndicatorModule,
+  DxSliderModule,
+  DxTextBoxModule,
+} from 'devextreme-angular';
 
 import { CommonModule } from '@angular/common';
 import { ControlBarComponent } from './video-player/control-bar/control-bar.component';
 import { NgModule } from '@angular/core';
 import { PlayControlComponent } from './video-player/play-control/play-control.component';
+import { SearchBarComponent } from './video-grid/search-bar/search-bar.component';
 import { TimeComponent } from './video-player/time/time.component';
 import { VideoComponent } from './video.component';
 import { VideoGridComponent } from './video-grid/video-grid/video-grid.component';
@@ -12,7 +18,6 @@ import { VideoRoutingModule } from './video-routing.module';
 import { VideoSeekerComponent } from './video-player/video-seeker/video-seeker.component';
 import { VideoWrapperComponent } from './video-player/video-wrapper/video-wrapper.component';
 import { VolumeControlComponent } from './video-player/volume-control/volume-control.component';
-import { SearchBarComponent } from './video-grid/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +32,13 @@ import { SearchBarComponent } from './video-grid/search-bar/search-bar.component
     VideoGridComponent,
     SearchBarComponent,
   ],
-  imports: [CommonModule, VideoRoutingModule, DxSliderModule, DxButtonModule, DxTextBoxModule],
+  imports: [
+    CommonModule,
+    VideoRoutingModule,
+    DxSliderModule,
+    DxButtonModule,
+    DxTextBoxModule,
+    DxLoadIndicatorModule,
+  ],
 })
 export class VideoModule {}
