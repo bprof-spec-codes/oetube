f41ad05a68916d4f0505748ba22be85a93bd4d6e
beresB <57649310+beresB@users.noreply.github.com>
2023-10-22 15:42:01 +0200

    VideoDto added

---
+++ oetube/aspnet-core/OeTube/Application/Dtos/Videos/VideoDto.cs
@@ -0,0 +1,25 @@
+﻿using OeTube.Domain.Entities.Videos;
+
+namespace OeTube.Application.Dtos.Videos
+{
+    public class ResolutionSrcDto
+    {
+        public Resolution Resolution { get; set; } = Resolution.Zero;
+        public string Src { get; set; } = string.Empty;
+    }
+    public class VideoDto
+    {
+        public Guid Id { get; set; }
+        public List<ResolutionSrcDto> ResolutionsSrc { get; set; } = new();
+        public string IndexImageSrc { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public Guid? CreatorId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public List<Guid> AccessGroups { get; set; } = new List<Guid>();
+        public Guid? PlaylistId { get; set; }
+        public bool IsUploadCompleted { get; set; }
+    }
+
+}
