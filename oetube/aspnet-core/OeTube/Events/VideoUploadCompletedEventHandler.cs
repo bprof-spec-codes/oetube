4f1181bee2688cbe05e5e00efa7425ad9204bd24
beresB <57649310+beresB@users.noreply.github.com>
2023-10-22 15:39:07 +0200

    VideoUploadCompletedEventHandler added

---
+++ oetube/aspnet-core/OeTube/Events/VideoUploadCompletedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using OeTube.Domain.Entities.Videos;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus;
+
+namespace OeTube.Events
+{
+    public class VideoUploadCompletedEto
+    {
+        public Video Video { get; }
+        public VideoUploadCompletedEto(Video video)
+        {
+            Video = video;
+        }
+    }
+
+    public class VideoUploadCompletedEventHandler : ILocalEventHandler<VideoUploadCompletedEto>, ITransientDependency
+    {
+        public Task HandleEventAsync(VideoUploadCompletedEto eventData)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
