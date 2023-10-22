b0dd38a3ce49189981e7d21005553b7e5eab24ff
beresB <57649310+beresB@users.noreply.github.com>
2023-10-22 15:33:30 +0200

    new WithNew*() functions added

--- oetube/aspnet-core/OeTube/Infrastructure/ByteContent.cs
+++ oetube/aspnet-core/OeTube/Infrastructure/ByteContent.cs
@@ -8,13 +8,13 @@ namespace OeTube.Infrastructure
     {
         public static async Task<ByteContent> FromStreamAsync(string? name,Stream stream,CancellationToken cancellationToken=default)
         {
-            return new ByteContent(name,await stream.GetAllBytesAsync());
+            return new ByteContent(name,await stream.GetAllBytesAsync(cancellationToken));
         }
         public static async Task<ByteContent> FromRemoteStreamContentAsync(IRemoteStreamContent content,CancellationToken cancellationToken=default)
         {
             return await FromStreamAsync(content.FileName, content.GetStream(), cancellationToken);
         }
-        public ByteContent(string? path, byte[] bytes)
+        public ByteContent(string? path, byte[] bytes,string? contentType=null)
         {
             if (string.IsNullOrWhiteSpace(path))
             {
@@ -23,27 +23,38 @@ namespace OeTube.Infrastructure
 
             Path = path;
             Bytes = bytes ?? throw new ArgumentNullException(nameof(bytes));
+            ContentType= contentType??MimeTypes.GetByExtension(Format);
         }
         public string Path { get; }
-        public string Name => System.IO.Path.GetFileName(Path);
         public string Format => System.IO.Path.GetExtension(Path).Trim('.');
         public byte[] Bytes { get; }
-        public string ContentType => MimeTypes.GetByExtension(Format);
-        public ByteContent WithNewPath(params string[] path)
+        public string ContentType { get; } 
+        public string FileName => System.IO.Path.GetFileName(Path);
+        public string FileNameWithoutFormat => System.IO.Path.GetFileNameWithoutExtension(Path);
+        public ByteContent WithNewName(string name)
         {
-            return new ByteContent(System.IO.Path.Combine(path), Bytes);
+            string path = System.IO.Path.Combine(Path.Replace(FileName, ""),$"{name}.{Format}");
+            return new ByteContent(path, Bytes,ContentType);
+        }
+        public ByteContent WithNewPath(string path)
+        {
+            return new ByteContent(path, Bytes,ContentType);
         }
         public ByteContent WithNewFormat(string format)
         {
             return WithNewPath(System.IO.Path.ChangeExtension(Path, format));
         }
+        public ByteContent WithNewContentType(string contentType)
+        {
+            return new ByteContent(Path, Bytes, contentType);
+        }
         public Stream GetStream()
         {
             return new MemoryStream(Bytes,true);
         }
-        public IRemoteStreamContent GetRemoteStreamContent()
+        public IRemoteStreamContent GetRemoteStreamContent(string? contentType=null)
         {
-            return new RemoteStreamContent(GetStream(), Path, ContentType);
+            return new RemoteStreamContent(GetStream(), Path, contentType??ContentType);
         }
 
     }
