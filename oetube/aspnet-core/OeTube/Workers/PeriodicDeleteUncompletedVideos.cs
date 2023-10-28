9acd43752af40129f7a2674807a0600a120b41f6
beresB <57649310+beresB@users.noreply.github.com>
2023-10-28 17:27:01 +0200

    PeriodicDeleteUncompletedVideos background worker added

---
+++ oetube/aspnet-core/OeTube/Workers/PeriodicDeleteUncompletedVideos.cs
@@ -0,0 +1,29 @@
+﻿using OeTube.Domain.Managers;
+using Volo.Abp.BackgroundWorkers;
+using Volo.Abp.Threading;
+
+namespace OeTube.Workers
+{
+    public class PeriodicDeleteUncompletedVideos : AsyncPeriodicBackgroundWorkerBase
+    {
+        private readonly TimeSpan period = TimeSpan.FromHours(6);
+
+        public PeriodicDeleteUncompletedVideos(AbpAsyncTimer timer, IServiceScopeFactory serviceScopeFactory) : base(timer, serviceScopeFactory)
+        {
+            Timer.Period = period.Milliseconds;
+        }
+
+        protected override async Task DoWorkAsync(PeriodicBackgroundWorkerContext workerContext)
+        {
+            Logger.LogInformation("Starting: " + GetType().Name);
+
+            var videoManager = workerContext
+                .ServiceProvider
+                .GetRequiredService<VideoManager>();
+
+            await videoManager.DeleteUncompletedVideosAsync(period);
+
+            Logger.LogInformation("Completed: " + GetType().Name);
+        }
+    }
+}
