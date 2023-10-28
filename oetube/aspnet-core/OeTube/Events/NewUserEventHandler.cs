ffa702c9d8f49fa697d680c2b7a557347754b749
beresB <57649310+beresB@users.noreply.github.com>
2023-10-28 17:28:15 +0200

    small changes and auto code formating in events

--- oetube/aspnet-core/OeTube/Events/NewUserEventHandler.cs
+++ oetube/aspnet-core/OeTube/Events/NewUserEventHandler.cs
@@ -2,25 +2,23 @@
 using OeTube.Entities;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Entities.Events;
-using Volo.Abp.Domain.Entities.Events.Distributed;
-using Volo.Abp.Domain.Repositories;
 using Volo.Abp.EventBus;
 using Volo.Abp.Identity;
 
 namespace OeTube.Events
 {
-    public class NewUserEventHandler : ILocalEventHandler<EntityCreatedEventData<IdentityUser>>,ITransientDependency
+    public class NewUserEventHandler : ILocalEventHandler<EntityCreatedEventData<IdentityUser>>, ITransientDependency
     {
-        private readonly IOeTubeUserRepository _users;
+        private readonly IUserRepository _userRepository;
 
-        public NewUserEventHandler(IOeTubeUserRepository users)
+        public NewUserEventHandler(IUserRepository userRepository)
         {
-            _users = users;
+            _userRepository = userRepository;
         }
 
         public async Task HandleEventAsync(EntityCreatedEventData<IdentityUser> eventData)
         {
-            await _users.InsertAsync(new OeTubeUser(eventData.Entity),true);
+            await _userRepository.InsertAsync(new OeTubeUser(eventData.Entity), true);
         }
     }
 }
