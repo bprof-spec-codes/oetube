1e50396d32577d5c653ad1e8863e04df27c3b002
beresB <57649310+beresB@users.noreply.github.com>
2023-10-29 18:13:27 +0100

    FFmpeg replaced

---
+++ oetube/aspnet-core/OeTube/Domain/Infrastructure/FFmpeg/IFFmpegService.cs
@@ -0,0 +1,20 @@
+﻿using OeTube.Domain.Infrastructure.Videos;
+
+namespace OeTube.Domain.Infrastructure.FFmpeg
+{
+    public interface IFFmpegService
+    {
+        Guid Id { get; }
+        bool WriteToDebug { get; set; }
+
+        Task CleanUpAsync(CancellationToken cancellationToken = default);
+
+        Task<FFmpegResult> ExecuteAsync(ByteContent? input, string arguments, string? processName = null, CancellationToken cancellationToken = default);
+
+        Task<bool> DeleteAsync(string name, CancellationToken cancellationToken = default);
+
+        Task<ByteContent> GetContentAsync(string name, CancellationToken cancellationToken = default);
+
+        HashSet<string> GetFiles();
+    }
+}
