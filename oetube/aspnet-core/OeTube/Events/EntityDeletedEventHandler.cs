41b4a5daa1dbb42b64a628e78861b56afa0858e9
beresB <57649310+beresB@users.noreply.github.com>
2023-10-29 18:18:13 +0100

    EntityDeletedEventHandlers added

---
+++ oetube/aspnet-core/OeTube/Events/EntityDeletedEventHandler.cs
@@ -0,0 +1,46 @@
+﻿using OeTube.Domain.Entities.Groups;
+using OeTube.Domain.Entities.Videos;
+using OeTube.Domain.Infrastructure.FileContainers;
+using OeTube.Entities;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Entities.Events;
+using Volo.Abp.EventBus;
+
+namespace OeTube.Events
+{
+    public abstract class EntityDeletedEventHandler<TEntity,TKey> : ILocalEventHandler<EntityDeletedEventData<TEntity>>
+        where TEntity:IEntity<TKey>
+        where TKey:notnull
+    {
+        private readonly IFileContainer FileContainer;
+
+        public EntityDeletedEventHandler(IFileContainerFactory factory)
+        {
+            FileContainer=factory.Create<TEntity>();
+        }
+
+        public async virtual Task HandleEventAsync(EntityDeletedEventData<TEntity> eventData)
+        {
+            await FileContainer.DeleteKeyAsync(eventData.Entity.Id);
+        }
+    }
+    public class VideoDeletedEventHandler : EntityDeletedEventHandler<Video, Guid>,ITransientDependency
+    {
+        public VideoDeletedEventHandler(IFileContainerFactory factory) : base(factory)
+        {
+        }
+    }
+    public class UserDeletedEventHandler : EntityDeletedEventHandler<OeTubeUser, Guid>, ITransientDependency
+    {
+        public UserDeletedEventHandler(IFileContainerFactory factory) : base(factory)
+        {
+        }
+    }
+    public class GroupDeletedEventHandler : EntityDeletedEventHandler<Group, Guid>, ITransientDependency
+    {
+        public GroupDeletedEventHandler(IFileContainerFactory factory) : base(factory)
+        {
+        }
+    }
+}
