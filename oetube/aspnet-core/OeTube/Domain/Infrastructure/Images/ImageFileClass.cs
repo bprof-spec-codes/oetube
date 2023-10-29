629d906e9b477d606e037e934816d56b17bcf6ba
beresB <57649310+beresB@users.noreply.github.com>
2023-10-29 18:17:32 +0100

    ImageFileClass added

---
+++ oetube/aspnet-core/OeTube/Domain/Infrastructure/Images/ImageFileClass.cs
@@ -0,0 +1,17 @@
+﻿using OeTube.Domain.Infrastructure.FileContainers;
+
+namespace OeTube.Domain.Infrastructure.Images
+{
+    public class ImageFileClass : FileClass
+    {
+        public override string Key { get; }
+        public override string Name => "image";
+        public override string MimeTypeCategory => "image";
+
+
+        public ImageFileClass(Guid id)
+        {
+            Key = id.ToString();
+        }
+    }
+}
