7d4ce92f52af1bc3533a4c1aee5fee4ec089352c
beresB <57649310+beresB@users.noreply.github.com>
2023-11-02 06:40:42 +0100

    configs added

---
+++ oetube/aspnet-core/OeTube/Configs/ThumbnailImageFileConfig.cs
@@ -0,0 +1,22 @@
+﻿using OeTube.Domain.Configs;
+using Volo.Abp.DependencyInjection;
+
+namespace OeTube.Configs
+{
+ 
+    public class ThumbnailImageFileConfig : IThumbnailImageFileConfig,ISingletonDependency
+    {
+        public ThumbnailImageFileConfig()
+        {
+            MaxWidth = 300;
+            MaxHeight =300;
+            OutputFormat = "jpg";
+        }
+
+        public int MaxWidth { get; }
+        public int MaxHeight { get; }
+        public string OutputFormat { get; }
+
+
+    }
+}
---
+++ oetube/aspnet-core/OeTube/Domain/Configs/IImageFileConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace OeTube.Domain.Configs
+{
+    public interface IImageFileConfig
+    {
+        public int MaxWidth { get; }
+        public int MaxHeight { get; }
+        public string OutputFormat { get; }
+    }
+    public interface ISourceImageFileConfig:IImageFileConfig
+    {
+     
+    }
+    public interface IThumbnailImageFileConfig:IImageFileConfig
+    {
+
+    }
+}
