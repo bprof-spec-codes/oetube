835726d79fd7ca46210a3d65ff2bf06a75e4097b
Benjámin Béres <beresb0@gmail.com>
2023-11-08 17:58:07 +0100

    PaginationResult added

---
+++ oetube/aspnet-core/OeTube/Domain/PaginationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace OeTube.Domain
+{
+    public class PaginationResult<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public int Page { get; set; }
+        public int PageCount { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
