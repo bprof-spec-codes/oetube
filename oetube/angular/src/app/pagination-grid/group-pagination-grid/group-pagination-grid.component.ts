9a544f79abcba2682b0903b63627e9d4609d3f5f
beresB <57649310+beresB@users.noreply.github.com>
2023-11-13 15:19:24 +0100

    group pagination component added

---
+++ oetube/angular/src/app/pagination-grid/group-pagination-grid/group-pagination-grid.component.ts
@@ -0,0 +1,36 @@
+import { Component,Input } from '@angular/core';
+import { GroupService } from '@proxy/application';
+import { GroupListItemDto, GroupQueryDto } from '@proxy/application/dtos/groups';
+import { LoadOptions } from 'devextreme/data';
+import { PaginationGridComponent } from '../pagination-grid.component';
+
+@Component({
+  selector: 'app-group-pagination-grid',
+  templateUrl: './group-pagination-grid.component.html',
+  styleUrls: ['./group-pagination-grid.component.scss']
+})
+export class GroupPaginationGridComponent 
+extends PaginationGridComponent<GroupService,GroupQueryDto,GroupListItemDto>
+{
+  @Input() showName:boolean=true
+  @Input() showId:boolean=true
+  @Input() showThumbnailImage:boolean=true
+  @Input() showCreationTime:boolean=true
+  @Input() showTotalMembersCount:boolean=true
+  @Input() showIsMember:boolean=true
+  @Input() showCreator:boolean=true
+  
+  constructor(groupService:GroupService){
+    super()
+    this.listProvider=groupService
+  }
+  test(e:GroupListItemDto){
+  }
+  handleFilter(options: LoadOptions): void {
+  
+    this.queryArgs.name = this.findFilterValue(options.filter, "contains", "name")
+    this.queryArgs.creationTimeMin = this.findFilterValue(options.filter, ">=", "creationTime")
+    this.queryArgs.creationTimeMax = this.findFilterValue(options.filter, "<", "creationTime")
+    this.queryArgs.creatorId=this.findFilterValue(options.filter,"=","creatorId")
+  }
+}
