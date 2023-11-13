52bece31d429757a55828b9ec9bce58f260b3530
beresB <57649310+beresB@users.noreply.github.com>
2023-11-13 15:21:43 +0100

    imports changed

--- oetube/angular/src/app/app.module.ts
+++ oetube/angular/src/app/app.module.ts
@@ -15,8 +15,6 @@ import { ThemeBasicModule } from '@abp/ng.theme.basic';
 import { ThemeSharedModule } from '@abp/ng.theme.shared';
 import { environment } from '../environments/environment';
 import { registerLocale } from '@abp/ng.core/locale';
-import { GroupComponent } from './group/group.component';
-import { GroupCreateComponent } from './group/group-create/group-create.component';
 
 @NgModule({
   imports: [
