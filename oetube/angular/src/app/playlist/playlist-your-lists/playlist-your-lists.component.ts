a9515695f9ed46b9182c2f860f55fd891882c749
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2023-12-05 00:38:24 +0100

    current user queried and navigateToList implemented

--- oetube/angular/src/app/playlist/playlist-your-lists/playlist-your-lists.component.ts
+++ oetube/angular/src/app/playlist/playlist-your-lists/playlist-your-lists.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserDto } from '@proxy/application/dtos/oe-tube-users/models';
 import { PlaylistDto, PlaylistItemDto, PlaylistQueryDto } from '@proxy/application/dtos/playlists';
+import { OeTubeUserService } from '@proxy/application/oe-tube-user.service';
 import { PlaylistService } from '@proxy/application/playlist.service';
 import { ItemPerPage } from '@proxy/domain/repositories/query-args';
+import { ConfigStateService } from '@abp/ng.core';
+
 
 @Component({
   selector: 'app-playlist-your-lists',
@@ -16,8 +21,16 @@ export class PlaylistYourListsComponent implements OnInit {
     itemPerPage:ItemPerPage.P50,
     page:0
   }
+  userData : any = {}
+  itemTemplate(itemData: any, index: number, element: any){
+    let ownerClass = itemData.creator.id === this.userData.id.toString() ? 'owned-playlist' : ''
+    element.innerHTML = `<div class="${ownerClass}">${itemData.name}</div>`;
+  }
   
-  constructor(private playlistService : PlaylistService) {
+  constructor(private playlistService : PlaylistService,
+              private userService : OeTubeUserService,
+              private router : Router,
+              private config : ConfigStateService) {
   }
 
   ngOnInit(): void {
@@ -27,9 +40,19 @@ export class PlaylistYourListsComponent implements OnInit {
         this.playlists = success.items
       }
     })
+    this.userData = this.config.getOne("currentUser")
+    console.log(this.userData)
   }
 
   navigateToList(event){
-    console.log(event.itemData)
+    let itemData : any = event.itemData
+    console.log(itemData)
+    //the route may vary according to the creator of the playlist content component
+    this.router.navigate(['playlist/contents/' + itemData.id])
+  }
+
+  deletePlaylist(event){
+    let itemData : any = event.itemData
+    this.playlistService.delete(itemData.id)
   }
 }
