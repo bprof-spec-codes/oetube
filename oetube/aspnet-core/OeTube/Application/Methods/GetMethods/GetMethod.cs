f7b156cba0fbb52c86459857dd8fa92f7fcdb363
beresB <57649310+beresB@users.noreply.github.com>
2023-12-05 05:24:16 +0100

    unused interface removed

--- oetube/aspnet-core/OeTube/Application/Methods/GetMethods/GetMethod.cs
+++ oetube/aspnet-core/OeTube/Application/Methods/GetMethods/GetMethod.cs
@@ -1,16 +1,14 @@
-﻿using OeTube.Domain.Repositories.CustomRepository;
+﻿using OeTube.Application.AuthorizationCheckers;
+using OeTube.Domain.Repositories.CustomRepository;
+using OeTube.Domain.Repositories.QueryArgs;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Entities;
 
 namespace OeTube.Application.Methods.GetMethods
 {
-    public interface IGetMethod<TKey, TOutputDto>
-    {
-        Task<TOutputDto> GetAsync(TKey id);
-    }
-
+   
     public class GetMethod<TEntity, TKey, TOutputDto> :
-            GetBaseMethod<TEntity, TKey>, IGetMethod<TKey, TOutputDto> where TEntity : class, IEntity<TKey>
+            GetBaseMethod<TEntity, TKey> where TEntity : class, IEntity<TKey>
     {
         public GetMethod(IAbpLazyServiceProvider serviceProvider, IReadRepository<TEntity, TKey> repository) : base(serviceProvider, repository)
         {
